e4b159ebf6f8c0832d7ed1d1f589fa33c946f1cb
Vinicius Santos <viniciusantosd.abreu@gmail.com>
2024-07-10 21:27:34 -0300

    iniciando-estudos;

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express' );
+const app = express();
+const port = 3000;
+
+//rota padrao para iniciar 
+
+app.get('/', (req,res)=>{
+res.send('HelloWorld!' )
+})
+
+//escutar a porta
+app.listen(port, () =>{
+    console.log(`Tudo certo, servidor acontecendo no http://localhost:${port}` );
+})
